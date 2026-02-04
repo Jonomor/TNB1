84ad2256ab28e525cf8ef8f64b03e5bab9fbafe7
Jonomor <120700848+Jonomor@users.noreply.github.com>
2026-02-04 04:11:38 -0500

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
+import './styles/mobile-optimizations.css';
 import { Section } from './components/Section';
 import { PricingCard } from './components/PricingCard';
 import { Button } from './components/Button';
